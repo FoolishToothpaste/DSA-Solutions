7e2abf98792f297328de42afb31c74c726f397a7
Soham Roy <sohamroy947@gmail.com>
2026-01-03 22:57:57 +0530

    DSADay3String4

---
+++ TwoCharacters.c
@@ -0,0 +1,173 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'alternate' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int alternate(char* s) {
+    int max_len = 0;
+    int used[26] = {0};
+
+    // Mark which characters appear in the string
+    for (int i = 0; s[i]; i++)
+        used[s[i] - 'a'] = 1;
+
+    // Try all pairs of characters
+    for (int c1 = 0; c1 < 26; c1++) {
+        if (!used[c1]) continue;
+        for (int c2 = c1 + 1; c2 < 26; c2++) {
+            if (!used[c2]) continue;
+
+            char last = 0;
+            int valid = 1, len = 0;
+
+            for (int i = 0; s[i]; i++) {
+                if (s[i] - 'a' == c1 || s[i] - 'a' == c2) {
+                    if (s[i] == last) {
+                        valid = 0;
+                        break;
+                    }
+                    last = s[i];
+                    len++;
+                }
+            }
+            if (valid && len > max_len) max_len = len;
+        }
+    }
+
+    return max_len;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int l = parse_int(ltrim(rtrim(readline())));
+
+    char* s = readline();
+
+    int result = alternate(s);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
